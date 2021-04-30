33903a510390cf3e3488a3746d104ba76ebf608e
Laura Munteanu <laura.tarita@gmail.com>
2021-04-30 23:43:48 +0200

    client: defined routes for sidebar menu

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AgGridModule } from 'ag-grid-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import {
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/admin/teachers/teachers-admin.component.ts
+++ client/src/app/components/admin/teachers/teachers-admin.component.ts
@@ -12,4 +12,17 @@ export class TeachersAdminComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  columnDefs = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price'}
+];
+
+rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+];
+
+
 }
